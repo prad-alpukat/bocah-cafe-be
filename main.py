0d079fca6d42c9474abe819c235d8a0b48a1a249
prad.alpukat <pradityaaldi.s@gmail.com>
2025-12-31 07:26:18 +0700

    Remove example environment file, update SQLAlchemy version, and delete startup script

--- main.py
+++ main.py
@@ -33,7 +33,3 @@ app.include_router(collection.router, prefix="/api/collections", tags=["Collecti
 @app.get("/")
 def read_root():
     return {"message": "Welcome to Bocah Cafe API"}
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
--- startup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-fastapi run main.py --host 0.0.0.0 --port 8000
